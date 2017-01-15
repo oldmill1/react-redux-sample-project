bde0f7f04ed3596ea16c10c98489bacdce4db801
Ankur Taxali <ataxali@gmail.com>
2017-01-14 21:37:44 -0500

    Implemented react-router

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App'
 import { Provider } from 'react-redux'
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path='/' component={App} />
+        </Router>
     </Provider>
 )
 
